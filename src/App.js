7e442cb1385b25d0fa66230f6180fc9ab193343f
singhbraj <braj333singh@gmail.com>
2020-06-13 12:41:22 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TodoList from './components/Todo-list';
+import EditTodo from './components/edit-todo';
+import CreateTodo from './components/create-todo.js';
+ 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container"> 
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     <Link to="/" className="navbar-brand">TodoList App</Link>
+     <div className="collapse navbar-collapse">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <Link to="/edit/:id" className="nav-link">Edit</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/create" className="nav-link">Create</Link>
+      </li>
+      </ul>
+      </div>
+  
+    </nav>
+    
+    <Route path="/" exact component = {TodoList} />
+    <Route  path="/edit/:id" component={EditTodo} />
+    <Route  path="/create" component={CreateTodo} />
     </div>
+    </Router>
+
   );
 }
 
